4f2491473c9a0fb33ed03e927964b0f62e20b489
neverDieAgain <rr4428310@gmail.com>
2021-11-02 20:49:30 +0530

    Repo Name changed

--- src/main/java/com/mycompany/razorpay_client/CreateOrder.java
+++ src/main/java/com/mycompany/razorpay_client/CreateOrder.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.razopay_client;
+package com.mycompany.razorpay_client;
 
 import com.razorpay.Order;
 import com.razorpay.RazorpayClient;
